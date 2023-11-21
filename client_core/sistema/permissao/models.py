2a3c68c4a3c619b410dea37ad849980d1db8b2a1
Leandri Junior <62294533+LeandriJunior@users.noreply.github.com>
2023-11-21 06:24:34 -0300

    feat(sistema): criação modulos de grupo e ponto funcao

--- client_core/sistema/permissao/models.py
+++ client_core/sistema/permissao/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+import core.models
+
+
 # Create your models here.
+
+class PontoFuncao(core.models.Log):
+    nome = models.CharField(max_length=255, primary_key=True)
+    nm_descritivo = models.CharField(max_length=512, null=True)
+    descricao = models.CharField(max_length=1024, null=True)
+    modulo = models.CharField(max_length=64, null=True)
+    ponto_funcao_pai = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True)
+    prioridade = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'sistema_pontofuncao'
+
+
+class Grupo(core.models.Log):
+    nome = models.CharField(max_length=255, primary_key=True)
+    nm_descritivo = models.CharField(max_length=512, null=True)
+    descricao = models.CharField(max_length=1024, null=True)
+    modulo = models.CharField(max_length=64, null=True)
+    grupo_pai = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True)
+    prioridade = models.IntegerField(null=True)
+    class Meta:
+        db_table = 'sistema_grupo'
+
+
+class GrupoPontoFuncao(core.models.Log):
+    grupo = models.ForeignKey('Grupo', on_delete=models.DO_NOTHING, null=True )
+    ponto_funcao = models.ForeignKey('PontoFuncao', on_delete=models.DO_NOTHING, null=True)
+
+    class Meta:
+        db_table = 'sistema_grupo_pontofuncao'
+
